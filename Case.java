a96202c743a04a884f4b32d8176e82ddc2186539
2TH25 <mattheo.girardclos@gmail.com>
2024-04-29 17:56:10 +0200

    yes

--- Case.java
+++ Case.java
@@ -1,3 +1,16 @@
 public class Case extends Plateau {
        
+    private Position position ;
+    private char symbole;
+
+    Case (Position position, char symbole) {
+        this.position = position;
+        this.symbole = symbole;
+    }
+
+    Case (Position position){
+        this.position = position;
+        this.symbole = ' ';
+    }
+    
 }
