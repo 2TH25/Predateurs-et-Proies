c97d27fd82fc59b86bb53ab4573616c7287d4900
MeloKiki <kiliangaillard25@gmail.com>
2024-04-29 20:53:45 +0200

    constructeurs du plateau

--- Plateau.java
+++ Plateau.java
@@ -1,5 +1,14 @@
 public class Plateau {
     private int colNumber;
     private int rowNumber;
+ 
     
+
+    Plateau(int colNumber, int rowNumber){
+        this.colNumber=colNumber;
+        this.rowNumber=rowNumber;
+    }
+    Plateau(){
+        this(8,8);
+    }
 }
