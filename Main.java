910482cd15b9df3bb818d69116ec55b043bb916f
MeloKiki <kiliangaillard25@gmail.com>
2024-05-12 10:51:02 +0200

    stp fonctionne

--- Main.java
+++ Main.java
@@ -9,8 +9,8 @@ public class Main {
     //boucle de jeu principale
 
     while(PlateauDeJeu.nombreEntitesRestantes()>3){
-        for(int i = 0; i<PlateauDeJeu.getRowNumber()-1 ; i++){
-            for(int j = 0 ; j<PlateauDeJeu.getColNumber()-1 ; j++){
+        for(int i = 1; i<PlateauDeJeu.getRowNumber()-1 ; i++){
+            for(int j = 1 ; j<PlateauDeJeu.getColNumber()-1 ; j++){
                 if(PlateauDeJeu.getPlateau()[i][j].getSymbole()!='X'&& PlateauDeJeu.getPlateau()[i][j].getSymbole()!=' '){
                     PlateauDeJeu.getPlateau()[i][j].interactionCase(PlateauDeJeu.getPlateau()[ i + PlateauDeJeu.getPlateau()[i][j].getDirection().getRowDir()][ j + PlateauDeJeu.getPlateau()[i][j].getDirection().getColDir()]);
 
